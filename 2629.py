7d2cd97cdb6564bad76dc66b32521f6b53f0929f
minis <ministove3yo@gmail.com>
2022-08-02 00:08:08 +0900

    .

--- 2629.py
+++ 2629.py
@@ -2,12 +2,28 @@ weightNumber = int(input())
 weightList = list(map(int, input().split()))
 marbleNumber = int(input())
 marbleList = list(map(int, input().split()))
-DP = [[False] * 40001 for i in range(weightNumber)]
+if weightNumber > 0:
+    DP = [[False] * 15001 for i in range(weightNumber)]
+else:
+    DP = [[False] * 15001 for i in range(1)]
 prev_possibleList = [0]
 for i, weight in enumerate(weightList):
+    DP[i][0] = True
     possibleList = []
     for possible in prev_possibleList:
         if DP[i][(possible + weight)]:
             pass
-        elif (possible + weight) <= 400000:
-            possibleList =
+        elif (possible + weight) <= 15000:
+            possibleList.append(possible + weight)
+            DP[i][possible + weight] = True
+    for possible in prev_possibleList:
+        if DP[i][abs(possible - weight)]:
+            pass
+        elif abs(possible - weight) <= 15000:
+            possibleList.append(abs(possible - weight))
+            DP[i][abs(possible - weight)] = True
+    prev_possibleList.extend(possibleList)
+ans = []
+for marble in marbleList:
+    ans.append('Y' if marble <= 15000 and DP[-1][marble] else 'N')
+print(" ".join(ans))
