8501099a5dc1c7ba8a0c5f0d64255471b1b97aa0
Jaehak Kim <133801408+jaehakworkland@users.noreply.github.com>
2023-06-21 12:33:41 -0400

    1

--- 1246.cpp
+++ 1246.cpp
@@ -1,13 +1,50 @@
-//
-// Created by minis on 2023-06-21.
-//
 #include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
 int main() {
 	int N, M;
-	int* prices = new int[N];
 	std::cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		std::cin >> prices[i];
+	int **Matrix;
+	Matrix = (int**) malloc(sizeof(int*) * N);
+	for (int i = 0; i < N; i++) {
+		Matrix[i] = (int*) malloc(sizeof(int) * M);
+		for (int j = 0; j < M; j++) {
+			scanf("%1d", &Matrix[i][j]);
+		}
 	}
-
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int num;
+			scanf("%1d", &num);
+			Matrix[i][j] += num;
+		}
+	}
+	int counter = 0;
+	for (int i = 0; i < std::max(N-2, 0); i++) {
+		for (int j = 0; j < std::max(M-2, 0); j++) {
+			if (Matrix[i][j] % 2) {
+				counter += 1;
+				Matrix[i][j] += 1;
+				Matrix[i+1][j] += 1;
+				Matrix[i+2][j] += 1;
+				Matrix[i][j+1] += 1;
+				Matrix[i+1][j+1] += 1;
+				Matrix[i+2][j+1] += 1;
+				Matrix[i][j+2] += 1;
+				Matrix[i+1][j+2] += 1;
+				Matrix[i+2][j+2] += 1;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (Matrix[i][j] % 2) {
+				counter = -1;
+				break;
+			}
+		}
+	}
+	std::cout << counter << std::endl;
+	return 0;
 }
