759983d72fb3137600296864292b970046a7101a
Jaehak Kim <ministove3yo@gmail.com>
2025-07-10 14:06:42 -0400

    refactor HashQueue implementation and improve food restaurant management logic

--- 25406.py
+++ 25406.py
@@ -2,139 +2,180 @@ from collections import deque
 from queue import PriorityQueue
 
 n = int(input())
-foods = input().split()
+foods = list(map(int, input().split()))
 
 food_restaurant = {}
 for i, food in  enumerate(foods):
-  item : deque = food_restaurant.get(int(food), deque())
+  item : deque = food_restaurant.get(food, deque())
   restaurant_num = i + 1
   item.append(restaurant_num)
-  restaurant_num[food] = item
+  food_restaurant[food] = item
 
 
 class HashQueue:
-  value_index_hashmap = {}
-  heap = []
+  def __init__(self, reversed):
+    self.heap = []
+    self.value_index_hashmap = {}
+    self.reversed = reversed
 
   def insert(self, key, value):
     if value in self.value_index_hashmap:
       print("duplicate value")
       return
-    index = len(self.heap)
-    self.value_index_hashmap[value] = index
+    idx = len(self.heap)
+    self.value_index_hashmap[value] = idx
     self.heap.append((key, value))
-    if len(self.heap) > 1:
-      self.up_heapify(index)
+    if idx > 0:
+      self.up_heapify(idx)
 
   def up_heapify(self, index):
-    me = self.heap[index]
-    my_key = me[0]
-    my_value = me[1]
-    parent_index = (index - 1) // 2
-    parent = self.heap[parent_index]
-    parent_key = parent[0]
-    parent_value = parent[1]
-    if parent_key < my_key:
-      self.heap[parent_index] = me
-      self.heap[index] = parent
-      self.value_index_hashmap[parent_value] = index
-      self.value_index_hashmap[my_value] = parent_index
-      self.up_heapify(parent_index)
+    while index > 0:
+      parent = (index - 1) // 2
+      print(self.heap[index])
+      key, val = self.heap[index]
+      pkey, pval = self.heap[parent]
+      if (self.reversed and key > pkey) or (not self.reversed and key < pkey):
+        self.heap[parent], self.heap[index] = self.heap[index], self.heap[parent]
+        self.value_index_hashmap[val] = parent
+        self.value_index_hashmap[pval] = index
+        index = parent
+      else:
+        break
 
   def down_heapify(self, index):
     size = len(self.heap)
-    me = self.heap[index]
-    my_key = me[0]
-    my_value = me[1]
-    
-    largest_index = index
+    while True:
+      left = 2 * index + 1
+      right = 2 * index + 2
+      best = index
 
-    left_child_index = 2 * index + 1
-    right_child_index = 2 * index + 2
+      if left < size:
+        lkey, _ = self.heap[left]
+        bkey, _ = self.heap[best]
+        if (self.reversed and lkey > bkey) or (not self.reversed and lkey < bkey):
+          best = left
 
-    if left_child_index < size and self.heap[left_child_index][0] > self.heap[largest_index][0]:
-      largest_index = left_child_index
+      if right < size:
+        rkey, _ = self.heap[right]
+        bkey, _ = self.heap[best]
+        if (self.reversed and rkey > bkey) or (not self.reversed and rkey < bkey):
+          best = right
 
-    if right_child_index < size and self.heap[right_child_index][0] > self.heap[largest_index][0]:
-      largest_index = right_child_index
-    largest_child = self.heap[largest_index]
-    largest_child_key = largest_child[0]
-    largest_child_value = largest_child[1]
+      if best == index:
+        break
 
-    if largest_index != index:
-      self.heap[index], self.heap[largest_index] = self.heap[largest_index], self.heap[index]
-      self.value_index_hashmap[my_value] = largest_index
-      self.value_index_hashmap[largest_child_value] = index
-      self.down_heapify(largest_index)
+      # swap
+      self.heap[index], self.heap[best] = self.heap[best], self.heap[index]
+      _, val = self.heap[index]
+      _, best_val = self.heap[best]
+      self.value_index_hashmap[val] = index
+      self.value_index_hashmap[best_val] = best
+      index = best
 
   def pop(self):
-    if len(self.heap) <= 0:
+    if not self.heap:
       print("empty")
-    target = self.heap[0]
-    target_index = 0
-    target_key = target[0]
-    target_value = target[1]
-    last = self.heap[-1]
-    last_index = len(self.heap) - 1
-    last_key = last[0]
-    last_value = last[1]
-    self.heap[target_index] = last
-    self.heap.pop()
-    del self.value_index_hashmap[target_value]
-    self.value_index_hashmap[last_value] = 0
-    self.down_heapify(target_index)
+      return None
+    if len(self.heap) == 1:
+      key, val = self.heap.pop()
+      del self.value_index_hashmap[val]
+      return (key, val)
+
+    root_key, root_val = self.heap[0]
+    last_key, last_val = self.heap.pop()
+    del self.value_index_hashmap[root_val]
+    self.heap[0] = (last_key, last_val)
+    self.value_index_hashmap[last_val] = 0
+
+    self.down_heapify(0)
+    return (root_key, root_val)
     
   def pop_by_value(self, value):
-    if len(self.heap) <= 0:
+    if not self.heap:
       print("empty")
-    target_index = self.value_index_hashmap[value]
-    target = self.heap[target_index]
-    target_key = target[0]
-    target_value = target[1]
-    last = self.heap[-1]
-    last_index = len(self.heap) - 1
-    last_key = last[0]
-    last_value = last[1]
-    self.heap[target_index] = last
-    self.heap.pop()
-    del self.value_index_hashmap[target_value]
-    self.value_index_hashmap[last_value] = 0
-    self.down_heapify(target_index)
+      return None
+    idx = self.value_index_hashmap.get(value)
+    if idx is None:
+      print("value not found")
+      return None
+
+    key, val = self.heap[idx]
+    last_key, last_val = self.heap.pop()
+    del self.value_index_hashmap[val]
+
+    # if we removed the last element, we're done
+    if idx == len(self.heap):
+      return (key, val)
+
+    # otherwise, fill the hole and re-heapify
+    self.heap[idx] = (last_key, last_val)
+    self.value_index_hashmap[last_val] = idx
+
+    # last element might need to go up or down
+    self.up_heapify(idx)
+    self.down_heapify(idx)
+    return (key, val)
 
   def get_top(self):
+    if not self.heap:
+      print("empty")
+      return None
     return self.heap[0]
 
 
 
 
-restaurant_number_queue = HashQueue()
-restaurant_count_queue = HashQueue()
+restaurant_number_queue = HashQueue(reversed=False)
+restaurant_count_queue = HashQueue(reversed=True)
 
-for food in food_restaurant.keys:
-  restaurant_deque : deque = restaurant_num[food]
+def popup_food(food):
+  restaurant_deque = food_restaurant[food]
+  restaurant_number_queue.pop_by_value(food)
+  next_min_restaurant = -1
+  if len(restaurant_deque) > 0:
+    next_min_restaurant = restaurant_deque.popleft()
+    restaurant_number_queue.insert(next_min_restaurant, food)
+
+  restaurant_count = restaurant_count_queue.pop_by_value(food)
+  restaurant_count_value = restaurant_count[0]
+  restaurant_count_queue.insert(restaurant_count_value - 1, food)
+  return next_min_restaurant
+
+
+for food in food_restaurant:
+  restaurant_deque : deque = food_restaurant[food]
   restaurant_count = len(restaurant_deque)
-  restaurant_count_queue.insert((restaurant_count, food))
+  restaurant_count_queue.insert(restaurant_count, food)
   restaurant_num = restaurant_deque.popleft()
-  restaurant_number_queue.insert((restaurant_num, food))
+  restaurant_number_queue.insert(restaurant_num, food)
   
 counter = n
 last_food = -1
 seq = []
+print(restaurant_number_queue.heap)
+print(restaurant_count_queue.heap)
 for i in range(n):
   max_count_food = restaurant_count_queue.get_top()
+  max_count_food_value = max_count_food[1]
   max_count_food_count = max_count_food[0]
   if max_count_food_count > counter // 2 + 1:
-    max_count_food_restaurant_food = max_count_food[1]
-    yes = restaurant_number_queue.pop_by_value(max_count_food_restaurant_food)
-    restaurant_num = restaurant_deque.popleft()
-    new_yes = (restaurant_deque[0], max_count_food_restaurant_food)
-    restaurant_number_queue.insert(new_yes)
+    restaurant_num = popup_food(max_count_food_value)
+    last_food = max_count_food_value
     seq.append(restaurant_num)
+    print(restaurant_num)
   else:
-    min_restaurant = restaurant_number_queue.peek()
-    min_restaurant_num = min_restaurant[0]
-    min_restaurant_food = min_restaurant[1]
-    
-    restaurant_number_queue.get_top()
-
-    
+    next_restaurant = restaurant_number_queue.get_top()
+    next_restaurant_num = next_restaurant[0]
+    next_restaurant_food = next_restaurant[1]
+    if next_restaurant_food == last_food:
+      restaurant_number_queue.pop()
+      next_restaurant = restaurant_number_queue.get_top()
+      next_restaurant_num = next_restaurant[0]
+      next_restaurant_food = next_restaurant[1]
+      restaurant_number_queue.insert(next_restaurant[0], next_restaurant[1])
+    print("next_min_restaurant", next_restaurant_food)
+    popup_food(next_restaurant_food)
+    last_food = next_restaurant_food
+    seq.append(next_restaurant_num)
+    print(next_restaurant_num)
+print(seq)
